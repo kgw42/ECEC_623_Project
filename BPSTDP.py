498bb6f690f0eb61c9b7b5788d5dc0980904cb9e
Kathrina Waugh <kgw42@drexel.edu>
2021-05-10 21:40:36 -0400

    Added start code

---
+++ BPSTDP.py
@@ -0,0 +1,88 @@
+"""
+Custom BPSTDP Learning Rule
+"""
+from bindsnet.learning import LearningRule
+
+from abc import ABC
+from typing import Union, Optional, Sequence
+
+import torch
+import numpy as np
+
+from bindsnet.network.topology import (
+    AbstractConnection,
+    Connection,
+    LocalConnection,
+)
+# from ..utils import im2col_indices
+
+class BPSTDP(LearningRule):
+    # language=rst
+    """
+    Simple STDP rule involving both pre- and post-synaptic spiking activity. By default,
+    pre-synaptic update is negative and the post-synaptic update is positive.
+    """
+
+    def __init__(
+        self,
+        connection: AbstractConnection,
+        nu: Optional[Union[float, Sequence[float]]] = None,
+        reduction: Optional[callable] = None,
+        weight_decay: float = 0.0,
+        **kwargs
+    ) -> None:
+        # language=rst
+        """
+        Constructor for ``PostPre`` learning rule.
+        :param connection: An ``AbstractConnection`` object whose weights the
+            ``PostPre`` learning rule will modify.
+        :param nu: Single or pair of learning rates for pre- and post-synaptic events.
+        :param reduction: Method for reducing parameter updates along the batch
+            dimension.
+        :param weight_decay: Constant multiple to decay weights by on each iteration.
+        """
+        super().__init__(
+            connection=connection,
+            nu=nu,
+            reduction=reduction,
+            weight_decay=weight_decay,
+            **kwargs
+        )
+
+        assert (
+            self.source.traces and self.target.traces
+        ), "Both pre- and post-synaptic nodes must record spike traces."
+
+        if isinstance(connection, (Connection, LocalConnection)):
+            self.update = self._connection_update
+        # Ignores the ConvConnection
+        else:
+            raise NotImplementedError(
+                "This learning rule is not supported for this Connection type."
+            )
+
+    def _connection_update(self, **kwargs) -> None:
+        # language=rst
+        """
+        Post-pre learning rule for ``Connection`` subclass of ``AbstractConnection``
+        class.
+        """
+        batch_size = self.source.batch_size
+
+        # Pre-synaptic update.
+        if self.nu[0]:
+            source_s = self.source.s.view(batch_size, -1).unsqueeze(2).float()
+            target_x = self.target.x.view(batch_size, -1).unsqueeze(1) * self.nu[0]
+            self.connection.w -= self.reduction(torch.bmm(source_s, target_x), dim=0)
+            del source_s, target_x
+
+        # Post-synaptic update.
+        if self.nu[1]:
+            target_s = (
+                self.target.s.view(batch_size, -1).unsqueeze(1).float() * self.nu[1]
+            )
+            source_x = self.source.x.view(batch_size, -1).unsqueeze(2)
+            self.connection.w += self.reduction(torch.bmm(source_x, target_s), dim=0)
+            del source_x, target_s
+
+        super().update()
--- main.py
+++ main.py
@@ -1,4 +1,104 @@
 """
-Authors:
-Date: 
+Authors: Kathrina Waugh, Isaac Deerwester, Pankil Chauhan
+Date: May 5, 2021
+
+SNN with IF Neurons for Iris Dataset.
 """
+
+import torch
+import matplotlib.pyplot as plt
+from bindsnet.network import Network
+from bindsnet.network.nodes import Input, LIFNodes, IFNodes
+from bindsnet.network.topology import Connection
+from bindsnet.network.monitors import Monitor
+from bindsnet.analysis.plotting import plot_spikes, plot_voltages
+from bindsnet.learning import PostPre
+from BPSTDP import BPSTDP
+from bindsnet.pipeline import BasePipeline
+
+from sklearn import datasets
+import numpy as np
+
+# import the dataset
+iris = datasets.load_iris()
+X = iris.data[:, :4]  # Take the first 4 features (Length and width of both petal and sepal)
+y = iris.target
+
+# Parameters
+
+epsilon = 4 #ms
+beta = 250
+hidden_thresh = 0.9   # pre
+delta_t = 1 # 1ms
+mu = 0.0005
+H = torch.randn(10,1500)
+output_thresh = 0.25 * H
+U = 0
+
+# Create the Network
+network = Network()
+
+# Create and add input, hidden and output layers.
+input_layer = Input(n=4, traces=True)
+hidden_layer = IFNodes(n=30, traces=True, thresh=hidden_thresh)
+output_layer = IFNodes(n=3, traces=True, thresh=output_thresh)
+
+network.add_layer(
+    layer=input_layer, name="Input"
+)
+network.add_layer(
+    layer=hidden_layer, name="Hidden"
+)
+network.add_layer(
+    layer=output_layer, name="Output"
+)
+
+# Create connection between input and hidden layers.
+"""
+Input to output layer uses 
+"""
+# TODO: CREATE A LEARNING RULE IN BINDSNET
+input_hidden = Connection(
+    source=input_layer,
+    target=hidden_layer,
+    w=0 * torch.randn(input_layer.n, hidden_layer.n),  # Initial weights - what did the paper use for initial weights e.g. w = 0.1 * size of the connection, i.e. torch.ones(4,30)
+) # uses wmax instead of w
+
+# nu parameter is the learning rate
+# norm - constant weight normalization
+
+network.add_connection(
+    connection=input_hidden, source="Input", target="Hidden"
+)
+
+# Create connection between hidden and output layers.
+hidden_output = Connection(
+    source=hidden_layer,
+    target=output_layer,
+    w=0, # TODO: USE UPDATED WEIGHTS FROM UPDATE RULE - DECLARE A GLOBAL SELF.WEIGHTS
+    update_rule=BPSTDP) #uses wmax instead of w
+
+network.add_connection(
+    connection=hidden_output, source="Hidden", target="Output"
+)
+
+# w = torch.zeros(3,3) # Recurrent weights proportional to sq(dist) between neurons
+# n_sqrt = int(np.sqrt(3))
+# for i in range(3):
+#     x1, y1 = i // n_sqrt, i % n_sqrt
+#     for j in range(3):
+#         x2, y2 = j //n_sqrt, j % n_sqrt
+#         dx, dy = x2 - x1, y2 - y1
+#         w[i, j] = dx *dx + dy * dy
+#     w[i, i] = 0.0
+# w = torch.pow(w/w.max(), 0.25)
+# w = start_inhib + w* max_inhib
+#
+# recurrent_output_conn = network.Connection()
+
+pipeline = BasePipeline(network, output="Output", time=1, delta=delta_t)
+
+while True:
+    pipeline.step()
+    if pipeline.done == True:
+        pipeline._reset()
